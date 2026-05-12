24f749c106a17af14f523002432711f422bdeb01
GaJaMy <mansa0805@gmail.com>
2026-05-12 09:48:08 +0900

    first commit

---
+++ src/main/java/com/liveclass/be_b/BeBApplication.java
@@ -0,0 +1,13 @@
+package com.liveclass.be_b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeBApplication.class, args);
+	}
+
+}
